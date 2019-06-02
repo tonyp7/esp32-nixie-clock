314b5a56666da265e6ca1478aa4ed57b11b90f45
tonyp7 <bubbathemaster@gmail.com>
2019-06-02 16:55:36 +0800

    removed i2c init (see i2c.h)

--- nixie_clock_code/main/ds3231.c
+++ nixie_clock_code/main/ds3231.c
@@ -117,20 +117,7 @@ float ds3231_get_temperature(){
 
 }
 
-void ds3231_i2c_init(){
-    int i2c_master_port = I2C_MASTER_NUM;
-    i2c_config_t conf;
-    conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = I2C_MASTER_SDA_IO;
-    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;
-    conf.scl_io_num = I2C_MASTER_SCL_IO;
-    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;
-    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
-    i2c_param_config(i2c_master_port, &conf);
-    i2c_driver_install(i2c_master_port, conf.mode,
-                       I2C_MASTER_RX_BUF_DISABLE,
-					   I2C_MASTER_TX_BUF_DISABLE, 0);
-}
+
 
 
 esp_err_t ds3231_set_time(const struct tm *timeinfo){
