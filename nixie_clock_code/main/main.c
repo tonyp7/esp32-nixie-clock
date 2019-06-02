0a3741abfcc79fdcd409a687137dc699137c3895
tonyp7 <bubbathemaster@gmail.com>
2019-06-02 19:45:56 +0800

    added @FozzTexx ws2812 code

--- nixie_clock_code/main/main.c
+++ nixie_clock_code/main/main.c
@@ -55,6 +55,7 @@ SOFTWARE.
 #include "i2c.h"
 #include "ds3231.h"
 #include "display.h"
+#include "ws2812.h"
 
 
 
@@ -71,6 +72,75 @@ void monitoring_task(void *pvParameter)
 		ESP_LOGI(TAG, "free heap: %d",esp_get_free_heap_size());
 		vTaskDelay( pdMS_TO_TICKS(30000) );
 	}
+}
+
+
+
+void rainbow_task(void *pvParameters)
+{
+  const uint8_t anim_step = 1;
+  const uint8_t anim_max = 16;
+  const uint8_t pixel_count = 64; // Number of your "pixels"
+  const uint8_t delay = 25; // duration between color changes
+  rgb_t color = ws2812_create_rgb(anim_max, 0, 0);
+  uint8_t step = 0;
+  rgb_t color2 = ws2812_create_rgb(anim_max, 0, 0);
+  uint8_t step2 = 0;
+  rgb_t *pixels;
+
+
+  pixels = malloc(sizeof(rgb_t) * pixel_count);
+
+  while (1) {
+    color = color2;
+    step = step2;
+
+    for (uint8_t i = 0; i < pixel_count; i++) {
+      pixels[i] = color;
+
+      if (i == 1) {
+        color2 = color;
+        step2 = step;
+      }
+
+      switch (step) {
+      case 0:
+        color.g += anim_step;
+        if (color.g >= anim_max)
+          step++;
+        break;
+      case 1:
+        color.r -= anim_step;
+        if (color.r == 0)
+          step++;
+        break;
+      case 2:
+        color.b += anim_step;
+        if (color.b >= anim_max)
+          step++;
+        break;
+      case 3:
+        color.g -= anim_step;
+        if (color.g == 0)
+          step++;
+        break;
+      case 4:
+        color.r += anim_step;
+        if (color.r >= anim_max)
+          step++;
+        break;
+      case 5:
+        color.b -= anim_step;
+        if (color.b == 0)
+          step = 0;
+        break;
+      }
+    }
+
+    ws2812_set_colors(pixel_count, pixels);
+
+    vTaskDelay( pdMS_TO_TICKS(delay) );
+  }
 }
 
 
@@ -104,6 +174,10 @@ void test_task(void *pvParameter){
 void app_main()
 {
 
+
+	ws2812_init();
+	xTaskCreate(rainbow_task, "ws2812 rainbow demo", 4096, NULL, 10, NULL);
+	return;
 
 	/* GPIO init for SPI transactions & GPIOs used to control the display */
 	ESP_ERROR_CHECK(display_init());
---
+++ nixie_clock_code/main/ws2812.h
@@ -0,0 +1,63 @@
+/*
+Copyright (c) 2019 Tony Pottier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+@file ws2812.h
+@author Tony Pottier
+@brief Provides a WS2812 "neopixel" software driver
+
+@see https://idyl.io
+@see https://github.com/tonyp7/esp32-nixie-clock
+
+This is based of FozzTexx's public domain code on WS2812
+@see https://github.com/FozzTexx/ws2812-demo
+
+*/
+
+#ifndef MAIN_WS2812_H_
+#define MAIN_WS2812_H_
+
+#include <stdint.h>
+
+#define WS2818_DATA_GPIO		23
+
+typedef union {
+  struct __attribute__ ((packed)) {
+    uint8_t r, g, b;
+  };
+  uint32_t num;
+} rgb_t;
+
+extern void ws2812_init();
+extern void ws2812_set_colors(unsigned int length, rgb_t *array);
+
+inline rgb_t ws2812_create_rgb(uint8_t r, uint8_t g, uint8_t b){
+	rgb_t v;
+
+	v.r = r;
+	v.g = g;
+	v.b = b;
+	return v;
+}
+
+
+
+
+#endif /* MAIN_WS2812_H_ */
