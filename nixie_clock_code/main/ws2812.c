cae84b772b4f21a56c15cfc2dcd9698b69445999
tonyp7 <bubbathemaster@gmail.com>
2019-06-05 09:07:40 +0800

    style consistency

--- nixie_clock_code/main/ws2812.c
+++ nixie_clock_code/main/ws2812.c
@@ -19,15 +19,15 @@
 #define DURATION	12.5 /* minimum time of a single RMT duration
 				in nanoseconds based on clock */
 
-#define PULSE_T0H	(  350 / (DURATION * DIVIDER));
-#define PULSE_T1H	(  900 / (DURATION * DIVIDER));
-#define PULSE_T0L	(  900 / (DURATION * DIVIDER));
-#define PULSE_T1L	(  350 / (DURATION * DIVIDER));
-#define PULSE_TRS	(50000 / (DURATION * DIVIDER));
+#define PULSE_T0H						(  350 / (DURATION * DIVIDER));
+#define PULSE_T1H						(  900 / (DURATION * DIVIDER));
+#define PULSE_T0L						(  900 / (DURATION * DIVIDER));
+#define PULSE_T1L						(  350 / (DURATION * DIVIDER));
+#define PULSE_TRS						(50000 / (DURATION * DIVIDER));
 
-#define MAX_PULSES	32
+#define MAX_PULSES						32
 
-#define RMTCHANNEL	0
+#define WS2812_RMT_CHANNEL				0
 
 typedef union {
   struct {
@@ -37,30 +37,30 @@ typedef union {
     uint32_t level1:1;
   };
   uint32_t val;
-} rmtPulsePair;
+} rmt_pulse_pair_t;
 
 static uint8_t *ws2812_buffer = NULL;
 static unsigned int ws2812_pos, ws2812_len, ws2812_half;
 static xSemaphoreHandle ws2812_sem = NULL;
 static intr_handle_t rmt_intr_handle = NULL;
-static rmtPulsePair ws2812_bits[2];
+static rmt_pulse_pair_t ws2812_bits[2];
 
-void ws2812_initRMTChannel(int rmtChannel)
+void ws2812_init_rmt_channel(int rmt_channel)
 {
   RMT.apb_conf.fifo_mask = 1;  //enable memory access, instead of FIFO mode.
   RMT.apb_conf.mem_tx_wrap_en = 1; //wrap around when hitting end of buffer
-  RMT.conf_ch[rmtChannel].conf0.div_cnt = DIVIDER;
-  RMT.conf_ch[rmtChannel].conf0.mem_size = 1;
-  RMT.conf_ch[rmtChannel].conf0.carrier_en = 0;
-  RMT.conf_ch[rmtChannel].conf0.carrier_out_lv = 1;
-  RMT.conf_ch[rmtChannel].conf0.mem_pd = 0;
+  RMT.conf_ch[rmt_channel].conf0.div_cnt = DIVIDER;
+  RMT.conf_ch[rmt_channel].conf0.mem_size = 1;
+  RMT.conf_ch[rmt_channel].conf0.carrier_en = 0;
+  RMT.conf_ch[rmt_channel].conf0.carrier_out_lv = 1;
+  RMT.conf_ch[rmt_channel].conf0.mem_pd = 0;
 
-  RMT.conf_ch[rmtChannel].conf1.rx_en = 0;
-  RMT.conf_ch[rmtChannel].conf1.mem_owner = 0;
-  RMT.conf_ch[rmtChannel].conf1.tx_conti_mode = 0;    //loop back mode.
-  RMT.conf_ch[rmtChannel].conf1.ref_always_on = 1;    // use apb clock: 80M
-  RMT.conf_ch[rmtChannel].conf1.idle_out_en = 1;
-  RMT.conf_ch[rmtChannel].conf1.idle_out_lv = 0;
+  RMT.conf_ch[rmt_channel].conf1.rx_en = 0;
+  RMT.conf_ch[rmt_channel].conf1.mem_owner = 0;
+  RMT.conf_ch[rmt_channel].conf1.tx_conti_mode = 0;    /* loop back mode */
+  RMT.conf_ch[rmt_channel].conf1.ref_always_on = 1;    /* use APB 80Mhz clock */
+  RMT.conf_ch[rmt_channel].conf1.idle_out_en = 1;
+  RMT.conf_ch[rmt_channel].conf1.idle_out_lv = 0;
 
   return;
 }
@@ -79,38 +79,36 @@ void ws2812_copy()
 
   if (!len) {
     for (i = 0; i < MAX_PULSES; i++)
-      RMTMEM.chan[RMTCHANNEL].data32[i + offset].val = 0;
+      RMTMEM.chan[WS2812_RMT_CHANNEL].data32[i + offset].val = 0;
     return;
   }
 
   for (i = 0; i < len; i++) {
     bit = ws2812_buffer[i + ws2812_pos];
     for (j = 0; j < 8; j++, bit <<= 1) {
-      RMTMEM.chan[RMTCHANNEL].data32[j + i * 8 + offset].val =
+      RMTMEM.chan[WS2812_RMT_CHANNEL].data32[j + i * 8 + offset].val =
 	ws2812_bits[(bit >> 7) & 0x01].val;
     }
     if (i + ws2812_pos == ws2812_len - 1)
-      RMTMEM.chan[RMTCHANNEL].data32[7 + i * 8 + offset].duration1 = PULSE_TRS;
+      RMTMEM.chan[WS2812_RMT_CHANNEL].data32[7 + i * 8 + offset].duration1 = PULSE_TRS;
   }
 
   for (i *= 8; i < MAX_PULSES; i++)
-    RMTMEM.chan[RMTCHANNEL].data32[i + offset].val = 0;
+    RMTMEM.chan[WS2812_RMT_CHANNEL].data32[i + offset].val = 0;
 
   ws2812_pos += len;
   return;
 }
 
-void ws2812_handleInterrupt(void *arg)
-{
-  portBASE_TYPE taskAwoken = 0;
-
+void ws2812_handle_interrupt(void *arg){
+	BaseType_t task_awoken = 0;
 
   if (RMT.int_st.ch0_tx_thr_event) {
     ws2812_copy();
     RMT.int_clr.ch0_tx_thr_event = 1;
   }
   else if (RMT.int_st.ch0_tx_end && ws2812_sem) {
-    xSemaphoreGiveFromISR(ws2812_sem, &taskAwoken);
+    xSemaphoreGiveFromISR(ws2812_sem, &task_awoken);
     RMT.int_clr.ch0_tx_end = 1;
   }
 
@@ -121,11 +119,11 @@ void ws2812_init(){
 	DPORT_SET_PERI_REG_MASK(DPORT_PERIP_CLK_EN_REG, DPORT_RMT_CLK_EN);
 	DPORT_CLEAR_PERI_REG_MASK(DPORT_PERIP_RST_EN_REG, DPORT_RMT_RST);
 
-	rmt_set_pin((rmt_channel_t)RMTCHANNEL, RMT_MODE_TX, (gpio_num_t)WS2818_DATA_GPIO);
+	rmt_set_pin((rmt_channel_t)WS2812_RMT_CHANNEL, RMT_MODE_TX, (gpio_num_t)WS2818_DATA_GPIO);
 
-	ws2812_initRMTChannel(RMTCHANNEL);
+	ws2812_init_rmt_channel(WS2812_RMT_CHANNEL);
 
-	RMT.tx_lim_ch[RMTCHANNEL].limit = MAX_PULSES;
+	RMT.tx_lim_ch[WS2812_RMT_CHANNEL].limit = MAX_PULSES;
 	RMT.int_ena.ch0_tx_thr_event = 1;
 	RMT.int_ena.ch0_tx_end = 1;
 
@@ -138,43 +136,42 @@ void ws2812_init(){
 	ws2812_bits[1].duration0 = PULSE_T1H;
 	ws2812_bits[1].duration1 = PULSE_T1L;
 
-	esp_intr_alloc(ETS_RMT_INTR_SOURCE, 0, ws2812_handleInterrupt, NULL, &rmt_intr_handle);
+	esp_intr_alloc(ETS_RMT_INTR_SOURCE, 0, ws2812_handle_interrupt, NULL, &rmt_intr_handle);
 
 	return;
 }
 
-void ws2812_set_colors(unsigned int length, rgb_t *array)
-{
-  unsigned int i;
+void ws2812_set_colors(unsigned int length, rgb_t *array){
+	unsigned int i;
 
 
-  ws2812_len = (length * 3) * sizeof(uint8_t);
-  ws2812_buffer = malloc(ws2812_len);
+	ws2812_len = (length * 3) * sizeof(uint8_t);
+	ws2812_buffer = malloc(ws2812_len);
 
-  for (i = 0; i < length; i++) {
-    ws2812_buffer[0 + i * 3] = array[i].g;
-    ws2812_buffer[1 + i * 3] = array[i].r;
-    ws2812_buffer[2 + i * 3] = array[i].b;
-  }
+	for (i = 0; i < length; i++) {
+		ws2812_buffer[i * 3 + 0] = array[i].g;
+		ws2812_buffer[i * 3 + 1] = array[i].r;
+		ws2812_buffer[i * 3 + 2] = array[i].b;
+	}
 
-  ws2812_pos = 0;
-  ws2812_half = 0;
+	ws2812_pos = 0;
+	ws2812_half = 0;
 
-  ws2812_copy();
+	ws2812_copy();
 
-  if (ws2812_pos < ws2812_len)
-    ws2812_copy();
+	if (ws2812_pos < ws2812_len)
+	ws2812_copy();
 
-  ws2812_sem = xSemaphoreCreateBinary();
+	ws2812_sem = xSemaphoreCreateBinary();
 
-  RMT.conf_ch[RMTCHANNEL].conf1.mem_rd_rst = 1;
-  RMT.conf_ch[RMTCHANNEL].conf1.tx_start = 1;
+	RMT.conf_ch[WS2812_RMT_CHANNEL].conf1.mem_rd_rst = 1;
+	RMT.conf_ch[WS2812_RMT_CHANNEL].conf1.tx_start = 1;
 
-  xSemaphoreTake(ws2812_sem, portMAX_DELAY);
-  vSemaphoreDelete(ws2812_sem);
-  ws2812_sem = NULL;
+	xSemaphoreTake(ws2812_sem, portMAX_DELAY);
+	vSemaphoreDelete(ws2812_sem);
+	ws2812_sem = NULL;
 
-  free(ws2812_buffer);
+	free(ws2812_buffer);
 
-  return;
+	return;
 }
