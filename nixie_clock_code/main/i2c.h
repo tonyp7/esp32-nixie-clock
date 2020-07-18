1629a2881bfbe119c7e3c4241f0cb8e2bbf14c83
tonyp7 <bubbathemaster@gmail.com>
2020-07-18 18:59:31 +0800

    updated headers

--- nixie_clock_code/main/i2c.h
+++ nixie_clock_code/main/i2c.h
@@ -1,9 +1,30 @@
 /*
- * i2c.h
- *
- *  Created on: 11 May 2019
- *      Author: tony
- */
+Copyright (c) 2020 Tony Pottier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+@file i2c.h
+@author Tony Pottier
+
+Contains helper functions to communicate over the I2C bus
+
+*/
 
 #ifndef MAIN_I2C_H_
 #define MAIN_I2C_H_
