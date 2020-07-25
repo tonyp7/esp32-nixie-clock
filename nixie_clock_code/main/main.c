13eeae587b6ef2051e77f594d2984d5a2be22007
tonyp7 <bubbathemaster@gmail.com>
2020-07-25 14:06:31 +0800

    experiments with SPI instead of RMT for WS2812

--- nixie_clock_code/main/main.c
+++ nixie_clock_code/main/main.c
@@ -55,6 +55,7 @@ SOFTWARE.
 #include "ds3231.h"
 #include "display.h"
 #include "ws2812.h"
+#include "webapp.h"
 
 
 
@@ -75,80 +76,136 @@ void monitoring_task(void *pvParameter)
 
 
 
+void bitcode(uint8_t r, uint8_t g, uint8_t b, uint8_t* buffer){
+  //order is G R B
 
+  //0 => 100
+  //1 => 110
 
-void rainbow_task(void *pvParameters)
-{
-  const uint8_t anim_step = 2;
-  const uint8_t anim_max = 250;
-  const uint8_t pixel_count = 6; // Number of your "pixels"
-  const uint8_t delay = 25; // duration between color changes
-  rgb_t color = ws2812_create_rgb(anim_max, 0, 0);
-  uint8_t step = 0;
-  rgb_t color2 = ws2812_create_rgb(anim_max, 0, 0);
-  uint8_t step2 = 0;
-  rgb_t *pixels;
+  uint8_t spidata[9];
+  uint32_t encoding = 0;
 
+  memset(spidata, 0, 9);
 
+  for(int i=0;i<8;i++){
 
-  pixels = malloc(sizeof(rgb_t) * pixel_count);
-
-  while (1) {
-    color = color2;
-    step = step2;
-
-    for (uint8_t i = 0; i < pixel_count; i++) {
-      pixels[i] = color;
-
-      if (i == 1) {
-        color2 = color;
-        step2 = step;
-      }
-
-      switch (step) {
-      case 0:
-        color.g += anim_step;
-        if (color.g >= anim_max)
-          step++;
-        break;
-      case 1:
-        color.r -= anim_step;
-        if (color.r == 0)
-          step++;
-        break;
-      case 2:
-        color.b += anim_step;
-        if (color.b >= anim_max)
-          step++;
-        break;
-      case 3:
-        color.g -= anim_step;
-        if (color.g == 0)
-          step++;
-        break;
-      case 4:
-        color.r += anim_step;
-        if (color.r >= anim_max)
-          step++;
-        break;
-      case 5:
-        color.b -= anim_step;
-        if (color.b == 0)
-          step = 0;
-        break;
-      }
+    if(g & (uint8_t)0x80){
+      encoding |= 0b110;
+    }
+    else{
+      encoding |= 0b110;
     }
 
-
-
-    //portMUX_TYPE myMutex = portMUX_INITIALIZER_UNLOCKED;
-
-    //portENTER_CRITICAL(&myMutex);
-    ws2812_set_colors(pixel_count, pixels);
-    //portEXIT_CRITICAL(&myMutex);
-
-    vTaskDelay( pdMS_TO_TICKS(delay) );
+    encoding <<= 3;
+    g <<= 1;
   }
+
+  spidata[0] = ((encoding >> 16) & 0xff);
+  spidata[1] = ((encoding >> 8) & 0xff);
+  spidata[2] = (encoding & 0xff);
+
+  encoding = 0;
+  for(int i=0;i<8;i++){
+
+    if(r & (uint8_t)0x80){
+      encoding |= 0b110;
+    }
+    else{
+      encoding |= 0b110;
+    }
+
+    encoding <<= 3;
+    r <<= 1;
+  }
+
+  spidata[3] = ((encoding >> 16) & 0xff);
+  spidata[4] = ((encoding >> 8) & 0xff);
+  spidata[5] = (encoding & 0xff);
+
+  encoding = 0;
+  for(int i=0;i<8;i++){
+
+    if(b & (uint8_t)0x80){
+      encoding |= 0b110;
+    }
+    else{
+      encoding |= 0b110;
+    }
+
+    encoding <<= 3;
+    b <<= 1;
+  }
+
+  spidata[6] = ((encoding >> 16) & 0xff);
+  spidata[7] = ((encoding >> 8) & 0xff);
+  spidata[8] = (encoding & 0xff);
+
+  memcpy(buffer, spidata, 9);
+  
+
+}
+
+void rainbow_task2(void *pvParameter){
+
+	//esp_err_t ret;
+
+  spi_device_handle_t spi;
+  spi_transaction_t t;
+
+	/* setup all GPIOs */
+	gpio_set_direction(WS2818_DATA_GPIO, GPIO_MODE_OUTPUT);
+
+	spi_bus_config_t buscfg={
+		.miso_io_num=-1, /* -1 == not used */
+		.mosi_io_num=WS2818_DATA_GPIO,
+		.sclk_io_num=-1,
+		.quadwp_io_num=-1,
+		.quadhd_io_num=-1
+	};
+	spi_device_interface_config_t devcfg={
+		.clock_speed_hz=3000000,					/* clock at 3Mhz => about 1000 frames per seconds! */
+		.mode=0,                    			/* SPI mode 0 */
+		.spics_io_num=-1,      /* CS pin CS GPIO pin for this device, or -1 if not used. */
+		.queue_size=4                           /* Queue size is not really relevant for the nixie display so its kept low */
+		//.pre_cb=ili_spi_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
+	};
+
+
+	/* Initialize the SPI bus */
+	ESP_ERROR_CHECK(spi_bus_initialize(VSPI_HOST, &buscfg, 2));
+
+	/* Attach the display to the SPI bus */
+	ESP_ERROR_CHECK(spi_bus_add_device(VSPI_HOST, &devcfg, &spi));
+
+	/* prepare transaction */
+
+  const size_t buff_len = ((6 * 24 * 3)/8);
+  uint8_t* buffer = malloc(  sizeof(uint8_t) *  buff_len);
+
+	memset(&t, 0x00, sizeof(t));
+  memset(buffer, 0x00, buff_len);
+
+	t.length = 6 * 24 * 3;  /* 6 digits, 24 bits per digits, 3 bit needed for each bit */
+	t.tx_buffer = buffer;
+	t.user = NULL;
+
+
+
+  bitcode(255, 0, 0, buffer);
+  bitcode(0, 255, 0, &buffer[9]);
+  bitcode(0, 0, 255, &buffer[18]);
+  bitcode(255, 0, 0, &buffer[27]);
+  bitcode(0, 255, 0, &buffer[36]);
+  bitcode(0, 0, 255, &buffer[45]);
+
+  for(;;){
+
+    ESP_ERROR_CHECK(spi_device_transmit(spi, &t));
+    vTaskDelay(pdMS_TO_TICKS(5000));
+  }
+
+
+
 }
 
 
@@ -185,27 +242,20 @@ void app_main()
 
 	/* GPIO/RMT init for the WS2812 driver */
 	ESP_ERROR_CHECK(ws2812_init());
-
-
-	xTaskCreatePinnedToCore(rainbow_task, "ws2812 rainbow demo", 4096, NULL, 10, NULL, 0);
-	//return;
-
+	
 	/* GPIO init for SPI transactions & GPIOs used to control the display */
 	ESP_ERROR_CHECK(display_init());
 
-//	xTaskCreate(&test_task, "test_task", 8192, NULL, CLOCK_TASK_PRIORITY, NULL);
-
 	/* start the wifi manager */
 	wifi_manager_start();
+  webapp_register_handlers();
 
 	/* register cb for internet connectivity */
 	wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &clock_notify_sta_got_ip);
 
-	/* clock */
-	xTaskCreatePinnedToCore(&clock_task, "clock_task", 8192, NULL, CLOCK_TASK_PRIORITY, NULL, 1);
+	/* clock task */
+	//xTaskCreatePinnedToCore(&clock_task, "clock_task", 8192, NULL, CLOCK_TASK_PRIORITY, NULL, 1);
+  xTaskCreatePinnedToCore(&clock_task, "clock_task", 16384, NULL, CLOCK_TASK_PRIORITY, NULL, 1);
 
-	/* your code should go here. Here we simply create a task on core 2 that monitors free heap memory */
-	xTaskCreatePinnedToCore(&monitoring_task, "monitoring_task", 2048, NULL, 1, NULL, 1);
-
-
+	//xTaskCreatePinnedToCore(&monitoring_task, "monitoring_task", 2048, NULL, 1, NULL, 1);
 }
