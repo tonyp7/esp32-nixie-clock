8abad19da989ead3fda7cdd272041b552cd884e4
tonyp7 <bubbathemaster@gmail.com>
2020-08-01 10:19:05 +0800

    more JSON fiddling

--- nixie_clock_code/main/webapp.c
+++ nixie_clock_code/main/webapp.c
@@ -32,12 +32,13 @@ Contains page logic handling web app functionalities
 #include <esp_system.h>
 #include <esp_http_server.h>
 #include <sys/param.h> /* for the MIN macro */
-#include "esp_err.h"
-#include "cJSON.h"
+#include <esp_err.h>
+#include <cJSON.h>
+#include <http_app.h>
 
-#include "http_app.h"
 #include "ws2812.h"
 #include "webapp.h"
+#include "clock.h"
 
 
 /**
@@ -72,6 +73,48 @@ const static char http_pragma_no_cache[] = "no-cache";
 const static char TAG[] = "webapp";
 
 
+
+/**
+ * @brief transform a sleepmodes_t struct into its json string representation
+ */
+static char* webapp_get_sleepmodes_json(sleepmodes_t sleepmodes){
+
+	char* str_json = NULL;
+
+	/* format as following:
+
+	{
+    "enabled":true,
+    "data":[
+        {"enabled":true,"days":3,"from":50400,"to":50460},
+        {"enabled":false,"days":0,"from":0,"to":0},
+        {"enabled":false,"days":0,"from":0,"to":0},
+        {"enabled":false,"days":0,"from":0,"to":0}
+	}
+	*/
+
+	cJSON *root = cJSON_CreateObject();
+	cJSON *data = cJSON_CreateArray();
+
+	cJSON_AddBoolToObject( root, "enabled", sleepmodes.enable_sleepmode );
+	for(int i=0; i < CLOCK_MAX_SLEEPMODES; i++){
+
+		cJSON *sleepmode = cJSON_CreateObject();
+		cJSON_AddBoolToObject( sleepmode, "enabled", sleepmodes.sleepmode[i].enabled );
+		cJSON_AddNumberToObject( sleepmode, "days", sleepmodes.sleepmode[i].days );
+		cJSON_AddNumberToObject( sleepmode, "from", sleepmodes.sleepmode[i].from );
+		cJSON_AddNumberToObject( sleepmode, "to", sleepmodes.sleepmode[i].to );
+
+		cJSON_AddItemToArray( data, sleepmode);
+	}
+	cJSON_AddItemToObject(root, "data", data);
+
+	str_json = cJSON_Print(root);
+	cJSON_Delete(root);
+
+	return str_json;
+}
+
 static esp_err_t webapp_get_hander(httpd_req_t *req){
 
     if(strcmp(req->uri, "/") == 0){
@@ -101,10 +144,75 @@ static esp_err_t webapp_get_hander(httpd_req_t *req){
         httpd_resp_set_hdr(req, http_cache_control_hdr, http_cache_control_cache);
 		httpd_resp_send(req, (char*)clock_css_start, clock_css_end - clock_css_start);
     }
+    else if(strcmp(req->uri, "/sleepmode/") == 0){
+
+        clock_config_t conf = clock_get_config();
+        char* str_json = webapp_get_sleepmodes_json( conf.sleepmodes );
+
+        if(str_json){
+
+            httpd_resp_set_status(req, http_200_hdr);
+            httpd_resp_set_type(req, http_content_type_json);
+            httpd_resp_set_hdr(req, http_cache_control_hdr, http_cache_control_no_cache);
+            httpd_resp_set_hdr(req, http_pragma_hdr, http_pragma_no_cache);
+            httpd_resp_send(req, str_json, strlen(str_json));
+
+            free(str_json);
+
+            return ESP_OK;
+        }
+        else{
+            httpd_resp_send_500(req);
+            return ESP_FAIL;
+        }
+    }
     else{
 
         httpd_resp_send_404(req);
     }
+
+    return ESP_OK;
+}
+
+/**
+ * @brief Parses a json string into a sleepmodes_t struct 
+ */
+static esp_err_t webapp_parse_sleemodes(char *content, sleepmodes_t *sleepmodes){
+
+    sleepmodes_t sm;
+    memset(&sm, 0x00, sizeof(sleepmodes_t));
+    
+    cJSON *json = cJSON_Parse(content);
+    if(json == NULL){
+
+        const char *error_ptr = cJSON_GetErrorPtr();
+        ESP_LOGE(TAG, "cJSON parsing error: %s", error_ptr);
+        return ESP_FAIL;
+    }
+
+    cJSON *enable_sleepmode = cJSON_GetObjectItemCaseSensitive(json, "enabled");
+    sm.enable_sleepmode = (bool)enable_sleepmode->valueint;
+
+    cJSON *data = cJSON_GetObjectItemCaseSensitive(json, "data");
+    cJSON *sleepmode;
+    int i = 0;
+
+    cJSON_ArrayForEach(sleepmode, data){
+        cJSON *enabled = cJSON_GetObjectItemCaseSensitive(sleepmode, "enabled");
+        cJSON *days = cJSON_GetObjectItemCaseSensitive(sleepmode, "days");
+        cJSON *from = cJSON_GetObjectItemCaseSensitive(sleepmode, "from");
+        cJSON *to = cJSON_GetObjectItemCaseSensitive(sleepmode, "to");
+
+        sm.sleepmode[i].enabled = (bool)enabled->valueint;
+        sm.sleepmode[i].days = days->valueint;
+        sm.sleepmode[i].from = from->valueint;
+        sm.sleepmode[i].to = to->valueint;
+        i++;
+        if(i >= CLOCK_MAX_SLEEPMODES) break;
+    }
+
+    cJSON_Delete(json);
+    *sleepmodes = sm;
 
     return ESP_OK;
 }
@@ -114,12 +222,86 @@ static esp_err_t webapp_post_handler(httpd_req_t *req){
 
     if(strcmp(req->uri, "/sleepmode/") == 0){
 
-        httpd_resp_set_status(req, http_200_hdr);
-        //httpd_resp_set_status(req, http_503_hdr);
-        //httpd_resp_set_status(req, http_400_hdr);
-        httpd_resp_set_type(req, http_content_type_json);
-        httpd_resp_set_hdr(req, http_cache_control_hdr, http_cache_control_no_cache);
-        httpd_resp_set_hdr(req, http_pragma_hdr, http_pragma_no_cache);
+
+        const size_t max_buffer_size = 511;
+        sleepmodes_t sleepmodes;
+        esp_err_t ret = ESP_OK;
+        
+        /** read body buffer. The message should be in this format:
+            {
+                "enabled":	false,
+                "data":	[{
+                        "enabled":	false,
+                        "days":	255,
+                        "from":	99999,
+                        "to":	99999
+                    }, {
+                        "enabled":	false,
+                        "days":	255,
+                        "from":	99999,
+                        "to":	99999
+                    }, {
+                        "enabled":	false,
+                        "days":	255,
+                        "from":	99999,
+                        "to":	99999
+                    }, {
+                        "enabled":	false,
+                        "days":	255,
+                        "from":	99999,
+                        "to":	99999
+                    }]
+            }
+         * that is to say never more than 400 chars.
+         * because this is pretty big to be on the stack
+         * it is heap allocated
+         */
+
+        /* Truncate if content length larger than the buffer */
+        size_t recv_size = MIN(req->content_len, max_buffer_size);
+        char* content = malloc(sizeof(char) * (recv_size+1));
+        memset(content, 0x00, (recv_size+1));
+
+
+        int read_count = httpd_req_recv(req, content, recv_size);
+        if (read_count <= 0) {  /* 0 return value indicates connection closed */
+            /* Check if timeout occurred */
+            if (read_count == HTTPD_SOCK_ERR_TIMEOUT) {
+                /* In case of timeout one can choose to retry calling
+                    * httpd_req_recv(), but to keep it simple, here we
+                    * respond with an HTTP 408 (Request Timeout) error */
+                httpd_resp_send_408(req);
+            }
+            else {
+                httpd_resp_send_500(req);
+            }
+
+            free(content);
+            return ESP_FAIL;
+        }
+
+        
+        /* processing -- build the sleepomodes_t object */
+        ret = webapp_parse_sleemodes(content, &sleepmodes);
+        if(ret == ESP_OK){
+
+            /* send sleepmode over to the clock */
+            clock_notify_new_sleepmodes(sleepmodes);
+
+            /* web answer */
+            httpd_resp_set_status(req, http_200_hdr);
+            httpd_resp_set_type(req, http_content_type_json);
+            httpd_resp_set_hdr(req, http_cache_control_hdr, http_cache_control_no_cache);
+            httpd_resp_set_hdr(req, http_pragma_hdr, http_pragma_no_cache);
+            httpd_resp_send(req, content, strlen(content));
+            free(content);
+            return ESP_OK;
+        }
+        else{
+            httpd_resp_send_408(req);
+            free(content);
+            return ret;
+        }
 
     }
     else if(strcmp(req->uri, "/backlights/") == 0){
